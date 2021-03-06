9731395b97db20584ab6263a9ea98c640930a193
Celina Louissaint <celinalouissaint@gmail.com>
2021-03-06 09:33:23 -0800

    issue with go back functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,23 @@
 let startBtn = document.querySelector("#start-quiz-btn");
 let descriptionContainer = document.querySelector(".descprition");
-let questionContainer = document.querySelector(".question-container")
-let quizListener = document.querySelector(".quiz-main")
-let countdown = document.querySelector(".timer-containter")
+let questionContainer = document.querySelector(".question-container");
+let finalScoreContainer =document.querySelector(".final-score-container");
+let finalScoreEl = document.querySelector("#final-score-p");
+let playerInput = document.querySelector("#high-score-input")
+let playerSubmit = document.querySelector("#high-score-submit");
+let highScoreContainer = document.querySelector(".high-score-container");
+let highScoreLink = document.querySelector(".high-score-link")
+let highScoreUl = document.querySelector(".high-score-el");
+let quizListener = document.querySelector(".quiz-main");
+let countdown = document.querySelector(".timer-containter");
+let displayMsg = document.querySelector(".display-message");
+let backBtn = document.querySelector("#back-to-begining");
+let clearScoreBtn = document.querySelector("#clear-high-score-btn")
 let score = 0;
 let questionIdCounter = 0;
-let currentQuestion = 0
+let currentQuestion = 0;
 let timeLeft = 120;
-
+let scoreList = [];
 
 let questionsArr = [
     {
@@ -23,26 +33,95 @@ let questionsArr = [
         options: ["<script name='xxx.js'>", "<script src='xxx.js'></script>", "<script href='xxx.js'></script>"]
     }
 ]
+// clear highScore
+function clear(){
+
+}
+
+// restart quiz
+function restart(){
+    descriptionContainer.style.display = "block";
+    // questionContainer.style.display = "none";
+    // finalScoreContainer.style.display = "none";
+    // highScoreContainer.style.display = "none";
+    score = 0;
+    timeLeft = 120;
+    // startQuiz();
+}
+
+// error message for player input div
+function displayMessage(type, message) {
+    displayMsg.textContent = message;
+    displayMsg.setAttribute('class', type);
+  }
 
 // timer function
-function timeRemaining() {
-    
-    let timeInterval = setInterval(function() {
-        if(timeLeft === 0){
-            clearInterval(timeInterval);
-        } else if(timeLeft > 1){
-            countdown.innerText = timeLeft + " seconds remaining";
-            timeLeft --;
-        } else {
-            countdown.innerText = timeLeft + " second remaining";
-            timeLeft --;
-        }
-     },1000);
-}
+// function timeRemaining() {
+//     let timeInterval = setInterval(function() {
+//         if(timeLeft === 0){
+//             recordScore();
+//             clearInterval(timeInterval);
+//         } else if(timeLeft > 1){
+//             countdown.innerText = timeLeft + " seconds remaining";
+//             timeLeft --;
+//         } else {
+//             countdown.innerText = timeLeft + " second remaining";
+//             timeLeft --;
+//         }
+//      },1000);
+// }
     
 
+function viewHighScore(){
+    // hide all other pages and make high score page visible
+    descriptionContainer.style.display = "none";
+    questionContainer.style.display = "none";
+    finalScoreContainer.style.display = "none";
+    highScoreContainer.style.display = "block";
+
+    // get scores from localstorage
+    scoreList = JSON.parse(localStorage.getItem("scoreList", JSON.stringify(scoreList)));
+    // iterate through score and create score list elements
+    for( let i = 0; i < scoreList.length; i++){
+        // make score elements to append to DOM
+    let scoreEL = document.createElement("li");
+    scoreEL.innerHTML = `${Math.floor([i] + 1)}. ${scoreList[i].player} - ${scoreList[i].finalScore}`;
+    scoreEL.className = "score-item";
+    highScoreUl.appendChild(scoreEL)
+    }
+}
+
 function recordScore(){
-    console.log(`Your final score is: ${score}`);
+    // display finalscore container 
+    finalScoreContainer.style.display = "block";
+    finalScoreEl.innerHTML = `Your final score is: ${score}!`
+
+    // submit sccor and store values in local storage
+    playerSubmit.addEventListener("click", function(event){
+        event.preventDefault();
+
+        let playerInitials = playerInput.value;
+        // Error handling for player input 
+        if(playerInitials === ""){
+            displayMessage("error", "Initials cannot be blank");
+        } else if( playerInitials.length > 3){
+            displayMessage("error", "Initials are too long, please enter valid input")
+        }
+
+        // save score to object 
+        let scoreObj = {
+            player: playerInitials,
+            finalScore: score,
+        };
+
+        // push score to scoreList array
+        scoreList.push(scoreObj);
+
+
+        localStorage.setItem("scoreList", JSON.stringify(scoreList));
+        // TODO it looks like its overwritting instead of adding to the end?
+        viewHighScore();
+    });
 }
 
 function selection(event) {
@@ -50,34 +129,34 @@ function selection(event) {
     if(currentQuestion === 0){
         // Checking for correct answer
         if(event.target.innerText === "Document Object Model"){
-            score += 20;
+            score += 100;
             console.log(score)
             // add to score
         } else {
             timeLeft -= 10;
-            score -= 5;
+            score -= 20;
             console.log("wrong")
             // TODO Subtract from Score and Time
         }
     } else if (currentQuestion === 1) {
         if(event.target.innerText === "<script>"){
-            score += 20;
+            score += 100;
             console.log(score)
         } else {
             // TODO Subtract from Score and Time
             timeLeft -= 10;
-            score -= 5;
+            score -= 20;
             console.log("wrong")
         }
     } else {
         if(event.target.innerText === "<script src='xxx.js'></script>"){
             console.log("third question");
-            score += 20;
+            score += 100;
             console.log(score);
             recordScore();
          } else{
             timeLeft -= 10;
-            score -= 5;
+            score -= 20;
             console.log("wrong")
             recordScore();
          };
@@ -92,7 +171,8 @@ function selection(event) {
 };
 
 function showQuestion(){
-    
+    // make question container visible
+    questionContainer.style.display = "block";
      // create div with class question-contaier and container-content
     let qContent = document.createElement("div");
     qContent.className = "question-contaier container-content";
@@ -115,7 +195,6 @@ function showQuestion(){
           btnContainer.appendChild(Btn);
          btnContainer.appendChild(Btn);
      }
-        
      qContent.appendChild(qH1);
      qContent.appendChild(btnContainer);
      // append div to the container
@@ -128,23 +207,20 @@ function showQuestion(){
 }
 
 
-let startQuiz = function(questionsArr){
+let startQuiz = function(){
     console.log("clicked")
-
     // hide description and make quiz visible 
     descriptionContainer.style.display = "none";
-    // make question container visible
-    questionContainer.style.display = "block";
-    // for each item in the questions object display the question and add/subtrack from score
-    timeRemaining();
+    // timeRemaining();
+    debugger;
+     
     showQuestion();
 }
 
 
-let showResults = function(){
-
-}
-
 
 startBtn.addEventListener("click", startQuiz);
 questionContainer.addEventListener("click", selection);
+highScoreLink.addEventListener("click", viewHighScore);
+backBtn.addEventListener("click", restart);
+clearScoreBtn.addEventListener("click", clear);
