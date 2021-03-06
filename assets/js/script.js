32f5f3ae6b816aec989b153ecb2b4f77a58112c7
Celina Louissaint <celinalouissaint@gmail.com>
2021-03-05 16:50:23 -0800

    create question and button elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,8 @@ let questionContainer = document.querySelector(".question-container")
 let quiz
 let score = 0;
 let questionIdCounter = 0;
+let currentQuestion = 0
+
 
 let questionsArr = [
     {
@@ -26,6 +28,54 @@ let questionsArr = [
     }
 ]
 
+
+
+function createQuestion(){
+     // for each item in the questions object display the question and add/subtrack from score
+    for(i = 0; i < questionsArr.length; i++){
+        // create div with class question-contaier and container-content
+        let qContent = document.createElement("div");
+        qContent.className = "question-contaier container-content";
+        // TODO I Am Here
+        qContent.setAttribute("data-q-id", questionIdCounter)
+        // create h1 class question
+        let qH1 = document.createElement("h1");
+        qH1.className = "questionH1";
+        qH1.innerHTML = questionsArr[i].question;
+        // create button with class q-option and data-q-id = [i]
+        let btnContainer = document.createElement("div")
+        btnContainer.className = "btnContainer"
+        let aBtn = document.createElement("button");
+        let bBtn = document.createElement("button");
+        let cBtn = document.createElement("button");
+        aBtn.className = "q-option";
+        bBtn.className = "q-option";
+        cBtn.className = "q-option";
+        aBtn.setAttribute("data-q-id", questionIdCounter);
+        bBtn.setAttribute("data-q-id", questionIdCounter);
+        cBtn.setAttribute("data-q-id", questionIdCounter);
+       
+        aBtn.innerText = questionsArr[i].a;
+        bBtn.innerText = questionsArr[i].b;
+        cBtn.innerText = questionsArr[i].c;
+        // append the question to the div and append the buttons to the div
+        btnContainer.appendChild(aBtn);
+        btnContainer.appendChild(bBtn);
+        btnContainer.appendChild(cBtn);
+
+        qContent.appendChild(qH1)
+        qContent.appendChild(btnContainer)
+        // append div to the container
+        questionContainer.appendChild(qContent)
+        // eventlistener and button functionality
+
+        // increase task counter for next unique id
+        questionIdCounter++
+        console.log(questionIdCounter)
+    }
+}
+
+
 let startQuiz = function(questionsArr){
     console.log("clicked")
     // hide description and make quiz visible 
@@ -33,21 +83,10 @@ let startQuiz = function(questionsArr){
     // make question container visible
     questionContainer.style.display = "block";
     // for each item in the questions object display the question and add/subtrack from score
-    for(i = 0; i < questionsArr.length; i++){
-        // create div with class question-contaier and container-content
-        let qContent = document.createElement("div");
-        qContent.className = "question-contaier container-content";
-        // TODO I Am Here
-        qContent.setAttribute("data-q-id", )
-        // create h1 class question
-        let qH1 = document.createElement("h1");
-        qH1.className = "questionH1";
-        // create button with class q-option and data-q-id = [i]
-
-        // append the question to the div and append the button to the div
-        // eventlistener and button functionality
-    }
+    createQuestion()
+    
 }
+
 
 let showResults = function(){
 
