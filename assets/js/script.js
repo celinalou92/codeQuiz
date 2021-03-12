57e75ee66e1cd7be9e13faa75173ddeb8b85bb53
Celina Louissaint <celinalouissaint@gmail.com>
2021-03-11 18:21:43 -0800

    test

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,11 +13,13 @@ let countdown = document.querySelector(".timer-containter");
 let displayMsg = document.querySelector(".display-message");
 let backBtn = document.querySelector("#back-to-begining");
 let clearScoreBtn = document.querySelector("#clear-high-score-btn")
+let qContent
 let score = 0;
 let questionIdCounter = 0;
 let currentQuestion = 0;
 let timeLeft = 120;
 let scoreList = [];
+countdown.innerText = timeLeft + " seconds";
 
 let questionsArr = [
     {
@@ -33,34 +35,37 @@ let questionsArr = [
         options: ["<script name='xxx.js'>", "<script src='xxx.js'></script>", "<script href='xxx.js'></script>"]
     }
 ]
-// clear highScore
-function clear(){
-
-}
-
-// restart quiz
-function restart(){
-    descriptionContainer.style.display = "block";
-    // questionContainer.style.display = "none";
-    // finalScoreContainer.style.display = "none";
-    // highScoreContainer.style.display = "none";
-    score = 0;
-    timeLeft = 120;
-    // startQuiz();
-}
-
-// error message for player input div
-function displayMessage(type, message) {
-    displayMsg.textContent = message;
-    displayMsg.setAttribute('class', type);
-  }
 
 // timer function
-// function timeRemaining() {
+
+// 2nd attempt 
+function timeInterval() {
+    console.log("timerstarted")
+    if(timeLeft === 0){
+        questionContainer.style.display = "none";
+        recordScore();
+        clearInterval(timeInterval)
+        countdown.innerHTML = `<h1> Out of time!</h1>`
+    } else if(timeLeft > 1){
+        countdown.innerText = timeLeft + " seconds remaining";
+        timeLeft --;
+    } else {
+        countdown.innerText = timeLeft + " second remaining";
+        timeLeft --;
+    }
+}
+
+// let timerId = setInterval(timeInterval, 1000)
+
+// 1st attempt commented out below
+
+// let timeRemaining = function() {
 //     let timeInterval = setInterval(function() {
 //         if(timeLeft === 0){
+//             questionContainer.style.display = "none";
 //             recordScore();
-//             clearInterval(timeInterval);
+//             clearInterval(timeInterval)
+//             countdown.innerHTML = `<h1> Out of time!</h1>`
 //         } else if(timeLeft > 1){
 //             countdown.innerText = timeLeft + " seconds remaining";
 //             timeLeft --;
@@ -70,6 +75,34 @@ function displayMessage(type, message) {
 //         }
 //      },1000);
 // }
+
+
+
+// clear highScore
+function clear(){
+
+}
+
+// restart quiz
+function restart(){
+    descriptionContainer.style.display = "block";
+    highScoreContainer.style.display = "none";
+    currentQuestion = 0;
+    questionIdCounter= 0;
+    score = 0;
+    timeLeft = 120;
+
+    startBtn.addEventListener("click", startQuiz);
+
+}
+
+// error message for player input div
+function displayMessage(type, message) {
+    displayMsg.textContent = message;
+    displayMsg.setAttribute('class', type);
+  }
+
+
     
 
 function viewHighScore(){
@@ -79,26 +112,20 @@ function viewHighScore(){
     finalScoreContainer.style.display = "none";
     highScoreContainer.style.display = "block";
 
-    // get scores from localstorage
-    scoreList = JSON.parse(localStorage.getItem("scoreList", JSON.stringify(scoreList)));
-    // iterate through score and create score list elements
+    highScoreUl.innerHTML = "";
+
     for( let i = 0; i < scoreList.length; i++){
         // make score elements to append to DOM
     let scoreEL = document.createElement("li");
-    scoreEL.innerHTML = `${Math.floor([i] + 1)}. ${scoreList[i].player} - ${scoreList[i].finalScore}`;
+    scoreEL.innerHTML = `${i + 1}. ${scoreList[i].player} - ${scoreList[i].finalScore}`;
     scoreEL.className = "score-item";
     highScoreUl.appendChild(scoreEL)
     }
 }
 
-function recordScore(){
-    // display finalscore container 
-    finalScoreContainer.style.display = "block";
-    finalScoreEl.innerHTML = `Your final score is: ${score}!`
-
-    // submit sccor and store values in local storage
-    playerSubmit.addEventListener("click", function(event){
-        event.preventDefault();
+ // submit scor and store values in local storage
+function submitScore(event){
+    event.preventDefault();
 
         let playerInitials = playerInput.value;
         // Error handling for player input 
@@ -107,7 +134,7 @@ function recordScore(){
         } else if( playerInitials.length > 3){
             displayMessage("error", "Initials are too long, please enter valid input")
         }
-
+        console.log(scoreList)
         // save score to object 
         let scoreObj = {
             player: playerInitials,
@@ -116,12 +143,18 @@ function recordScore(){
 
         // push score to scoreList array
         scoreList.push(scoreObj);
+        console.log(scoreList)
 
-
-        localStorage.setItem("scoreList", JSON.stringify(scoreList));
-        // TODO it looks like its overwritting instead of adding to the end?
         viewHighScore();
-    });
+}
+
+
+
+function recordScore(){
+    // display finalscore container 
+    finalScoreContainer.style.display = "block";
+    finalScoreEl.innerHTML = `Your final score is: ${score}!`
+    
 }
 
 function selection(event) {
@@ -149,33 +182,46 @@ function selection(event) {
             console.log("wrong")
         }
     } else {
+        console.log("Last Else")
         if(event.target.innerText === "<script src='xxx.js'></script>"){
             console.log("third question");
             score += 100;
             console.log(score);
-            recordScore();
-         } else{
+         } else {
             timeLeft -= 10;
             score -= 20;
             console.log("wrong")
-            recordScore();
          };
+         qContent = document.getElementById(questionIdCounter)
+         qContent.style.display = "none"
+         recordScore();
+         return
     };
+
     // hide the question just answered
-    qContent = document.getElementById(questionIdCounter)
+    qContent = document.getElementById(questionIdCounter);
     qContent.style.display = "none"
     // Increment the question id 
     currentQuestion++;
     questionIdCounter++;
-    showQuestion();
+    startQuiz();
 };
 
-function showQuestion(){
+
+
+function startQuiz(){
+    console.log("clicked")
+    // hide description and make quiz visible 
+    descriptionContainer.style.display = "none";
+
+    setInterval(timeInterval, 1000);
+
+    questionContainer.innerHTML = "";
     // make question container visible
     questionContainer.style.display = "block";
      // create div with class question-contaier and container-content
-    let qContent = document.createElement("div");
-    qContent.className = "question-contaier container-content";
+    qContent = document.createElement("div");
+    qContent.className = "container-content";
      // TODO I Am Here
     qContent.id = questionIdCounter;
      // create h1 class question
@@ -183,7 +229,7 @@ function showQuestion(){
     qH1.className = "questionH1";
     qH1.innerHTML = questionsArr[currentQuestion].question;
      // create button with class q-option and id = [i]
-    let btnContainer = document.createElement("div")
+    let btnContainer = document.createElement("div");
     btnContainer.className = "btnContainer"
 
      // for each item in the Object's options array create button element and append to the btnContainer
@@ -203,18 +249,7 @@ function showQuestion(){
      // increase task counter for next unique id
      console.log(`qContent id = ${questionIdCounter}`);
 
-    selection();
-}
-
-
-let startQuiz = function(){
-    console.log("clicked")
-    // hide description and make quiz visible 
-    descriptionContainer.style.display = "none";
-    // timeRemaining();
-    debugger;
-     
-    showQuestion();
+     return timerId
 }
 
 
@@ -224,3 +259,5 @@ questionContainer.addEventListener("click", selection);
 highScoreLink.addEventListener("click", viewHighScore);
 backBtn.addEventListener("click", restart);
 clearScoreBtn.addEventListener("click", clear);
+playerSubmit.addEventListener("click", submitScore)
+
